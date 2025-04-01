e7efbcbe928cb2cb516859ce705fab3b8bf52bbb
Apex <103858602+ApexTame@users.noreply.github.com>
2025-04-01 00:52:14 -0400

    Fixed Duped Code
    idk what happend but i fixed the duped code

--- src/features/clips/queue/VideoPlayer.tsx
+++ src/features/clips/queue/VideoPlayer.tsx
@@ -44,11 +44,8 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, onEnded }) => {
           ],
       techOrder: YouTube ? ['youtube', 'html5'] : ['html5'],
     });
-
-    if (playerRef.current) {
-      playerRef.current.volume(volume);
-    }
-
+    
+    //This Fixes The Black Bar Bug
     const resizePlayerOnSourceChange = () => {
       if (playerRef.current) {
         playerRef.current.fill(true);
@@ -59,18 +56,10 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, onEnded }) => {
       resizePlayerOnSourceChange();
     }
 
-    playerRef.current?.on('volumechange', () => {
-      const currentVolume = playerRef.current.volume();
-      dispatch(setVolume(currentVolume));
-    });
-
-    if (onEnded) {
-      playerRef.current?.on('ended', onEnded);
-    }
-
     if (playerRef.current) {
       playerRef.current.volume(volume);
     }
+    
     playerRef.current?.on('volumechange', () => {
       const currentVolume = playerRef.current.volume();
       dispatch(setVolume(currentVolume));
@@ -79,19 +68,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, onEnded }) => {
     if (onEnded) {
       playerRef.current?.on('ended', onEnded);
     }
-
-    if (playerRef.current) {
-      playerRef.current.volume(volume);
-    }
-    playerRef.current?.on('volumechange', () => {
-      const currentVolume = playerRef.current.volume();
-      dispatch(setVolume(currentVolume));
-    });
-
-    if (onEnded) {
-      playerRef.current?.on('ended', onEnded);
-    }
-
+    
     return () => {
       if (playerRef.current) {
         playerRef.current.dispose();
@@ -100,7 +77,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, onEnded }) => {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [src]);
-
+  
   return (
     <div data-vjs-player>
       <video ref={videoRef} className="video-js vjs-default-skin" />
